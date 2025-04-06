a4afd48fbe86a0c8cf198319498f8db48314b285
dakshSharma289 <dakshsharma784@gmail.com>
2025-04-06 15:30:27 +0530

    coppied checkPrime function from last mini proj because i dont know how to use directires yet, but the code now should work

--- printAllPrime.cpp
+++ printAllPrime.cpp
@@ -1,6 +1,27 @@
 #include<iostream>
 using namespace std;
 
+bool checkPrime(int num){
+    bool result = true;
+    for(int factor = 2; factor < num; factor++){
+        if(num % factor){
+            continue;
+        }else{
+            result = false;
+        }
+
+    }
+    return result;
+}
+
+void printAllPrime(int range){
+    for (int i = 1; i != range; i++){
+        if (checkPrime(i)){
+            cout << i;
+        }
+    }
+}
+
 int main(){
     return 0;
 }
