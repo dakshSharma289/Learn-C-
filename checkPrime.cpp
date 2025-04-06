623b2e759cfdbdda7917fcf04f071dc10ae73912
dakshSharma289 <dakshsharma784@gmail.com>
2025-04-06 15:03:19 +0530

    added a function called checkPrime and ivoked it to check if a number is prime or not

--- checkPrime.cpp
+++ checkPrime.cpp
@@ -1,7 +1,27 @@
 #include<iostream>
 using namespace std;
 
+bool checkPrime(int num){
+    bool result;
+    for(int factor = 2; factor < num; factor++){
+        if(num % factor){
+            continue;
+        }else{
+            result = false;
+        }
+    }
+    return result;
+}
+
 int main (){
+    int number;
+    cin >> number;
+
+    if (checkPrime(number)){
+        cout<< endl << "Is Prime" << endl;
+    }else{
+        cout << endl << "Is'nt Prime" << endl;
+    }
     
     return 0;
 }
